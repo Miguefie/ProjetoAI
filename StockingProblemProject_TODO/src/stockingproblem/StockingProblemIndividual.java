b40b393fdebaf85480e2189f382d57e03968c9e0
Miguel Fialho <2181430@my.ipleiria.pt>
2021-05-21 10:56:45 +0100

    GetIndividual Bug - FIXED

--- StockingProblemProject_TODO/src/stockingproblem/StockingProblemIndividual.java
+++ StockingProblemProject_TODO/src/stockingproblem/StockingProblemIndividual.java
@@ -22,13 +22,13 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
         }
         Random rn = new Random();
         //int randomItem = rn.nextInt(size+1); // Random do Item para adicionar no Genome (evitar o Valor 0)
-        this.genome[0] = itemList.get(rn.nextInt(itemList.size())); // Random (1...N)
-        System.out.println(itemList.get(this.genome[0]));
-        itemList.remove(itemList.get(this.genome[0]));
+        Integer itemAleatorio = itemList.get(rn.nextInt(itemList.size())); // Random (1...N)
+        this.genome[0] = itemAleatorio.intValue(); 
+        itemList.remove(itemAleatorio);
         for (int i = 1; i < this.genome.length; i++) {
-            int itemAleatorio = itemList.get(rn.nextInt(itemList.size()));
-            this.genome[i] = itemAleatorio; // Adiciona o Item aleatorio ao Genotipo
-            itemList.remove(itemList.get(this.genome[i]));
+            itemAleatorio = itemList.get(rn.nextInt(itemList.size()));
+            this.genome[i] = itemAleatorio.intValue(); // Adiciona o Item aleatorio ao Genotipo
+            itemList.remove(itemAleatorio);
         }
 
     }
@@ -42,17 +42,19 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
     @Override
     public double computeFitness() {
         //TODO
+        double cuts = 0;
         for (int k = 0; k < genome.length; k++) {
             Item itemAtual = problem.getItems().get(k);
             for (int i = 0; i < problem.getMaterialHeight(); i++) {  // Linhas Do Material
                 for (int j = 0; j < materialLength; j++) { // Colunas do Material (Vai ser o Tamanho das Pecas)
                     if(checkValidPlacement(itemAtual,material,i,j))
+                        cuts++;
                         material[i][j] = itemAtual.getRepresentation(); // Adiciona ao Fenotipo
                 }
             }
         }
 
-        return
+        return cuts;
     }
 
     private boolean checkValidPlacement(Item item, int[][] material, int lineIndex, int columnIndex) {
