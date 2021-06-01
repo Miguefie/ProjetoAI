f14ec5a7a2d7ee04d55cbce2acb13330041decf7
Indregan <tma.tico@hotmail.com>
2021-06-01 04:03:53 +0100

    randomINCOMPLETO
    - Não devolve melhor solução
    - Gráfico não funciona !!!!!!!!!

--- StockingProblemProject_TODO/src/random/RandomAlgorithm.java
+++ StockingProblemProject_TODO/src/random/RandomAlgorithm.java
@@ -1,13 +1,23 @@
 package random;
 
 import algorithms.Algorithm;
+import algorithms.AlgorithmEvent;
 import algorithms.Individual;
 import algorithms.Problem;
+import ga.GeneticAlgorithm;
+import ga.Population;
+import ga.geneticoperators.Mutation;
+import ga.geneticoperators.Recombination;
+import ga.selectionmethods.SelectionMethod;
+import gui.Main;
+import gui.MainFrame;
 
 import java.util.Random;
 
 public class RandomAlgorithm<I extends Individual, P extends Problem<I>> extends Algorithm<I, P> {
     //TODO this class might require the definition of additional methods and/or attributes
+    private Population<I, P> population;
+    //private final int populationSize;
 
     public RandomAlgorithm(int maxIterations, Random rand) {
         super(maxIterations, rand);
@@ -15,7 +25,37 @@ public class RandomAlgorithm<I extends Individual, P extends Problem<I>> extends
 
     @Override
     public I run(P problem) {
+
         //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        population = new Population<>(100, problem); //como sei populationSize??
+        globalBest = population.evaluate();
+        fireIterationEnded(new AlgorithmEvent(this));
+
+        for (int i = 0; i < maxIterations; i++) {
+            globalBest = randSearch(population);
+            fireIterationEnded(new AlgorithmEvent(this));
+        }
+
+        fireRunEnded(new AlgorithmEvent(this));
+        return globalBest; //Não devolve melhor solução
+    }
+
+    private I randSearch(Population<I, P> population) {
+
+        I aux = population.getIndividual(random.nextInt(population.getSize()));
+
+        if (aux.compareTo(globalBest) > 0) {
+            globalBest = (I) aux.clone();
+        }
+
+        return globalBest;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Population size:" + population.getSize() + "\n");
+        sb.append("Max generations:" + maxIterations + "\n");
+        return sb.toString();
     }
 }
