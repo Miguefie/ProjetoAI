195eff741820330549492fa805ed488aa3ea4297
Miguel Fialho <2181430@my.ipleiria.pt>
2021-06-06 00:52:33 +0100

    CrossOverRecombination

--- StockingProblemProject_TODO/src/ga/geneticoperators/Recombination3.java
+++ StockingProblemProject_TODO/src/ga/geneticoperators/Recombination3.java
@@ -12,22 +12,27 @@ public class Recombination3<I extends IntVectorIndividual, P extends Problem<I>>
 
     @Override
     public void recombine(I ind1, I ind2) {
-        //TODO
+        //TODO Cycle Crossover
         int[] child1 = new int[ind1.getNumGenes()];
         int[] child2 = new int[ind2.getNumGenes()];
-        int[] cycle = new int[ind1.getNumGenes()];
+        int[] cycle1 = new int[ind1.getNumGenes()];
+        int[] cycle2 = new int[ind1.getNumGenes()];
         for (int i = 0; i < ind1.getNumGenes(); i++) {
-            ind2.getGene(ind2.getIndexof(ind1.getGene(i)));
+            int positionAllele = ind2.getIndexof(ind1.getGene(i));
+            int allele = ind1.getGene(positionAllele);
+            cycle1[i]=allele;
+
+            positionAllele = ind1.getIndexof(ind2.getGene(i));
+            allele = ind2.getGene(positionAllele);
+            cycle2[i]=allele;
 
         }
-
-        do {
-            ind1.getGene(i);
-        }while();
+        child1 = cycle1;
+        child2 = cycle2;
     }
 
     @Override
     public String toString(){
-        return "Two cuts recombination (" + probability + ")";
+        return "Cycle Crossover (" + probability + ")";
     }    
 }
