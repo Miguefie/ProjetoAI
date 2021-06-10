2cd47aec7837a087a45d7f6e578517f609dc9125
Miguel Fialho <2181430@my.ipleiria.pt>
2021-06-10 11:51:31 +0100

    Rankbased Finished Start Experiments

--- StockingProblemProject_TODO/src/ga/selectionmethods/RankBased.java
+++ StockingProblemProject_TODO/src/ga/selectionmethods/RankBased.java
@@ -4,6 +4,9 @@ import algorithms.Individual;
 import algorithms.Problem;
 import ga.Population;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RankBased<I extends Individual, P extends Problem<I>> extends SelectionMethod<I, P> {
 
 
@@ -15,24 +18,30 @@ public class RankBased<I extends Individual, P extends Problem<I>> extends Selec
     public Population<I, P> run(Population<I, P> original) {
         Population<I, P> result = new Population<>(original.getSize());
         I bestIndividual = original.getBest();
+        List<I> selectedIndividuals = original.getIndividuals();
 
         for (int i = 0; i < popSize; i++) {
-            for (int j = 0; j < popSize; j++) {
-                
-                if(original.getIndividual(i).compareTo(original.getIndividual(j)) > 0)
+            int min = i;
+            for (int j = i; j < popSize; j++) {
+                if(selectedIndividuals.get(j).compareTo(selectedIndividuals.get(i)) < 0)
                 {
-
+                    min = j;
                 }
             }
-        }
+            I aux = selectedIndividuals.get(min);
+            selectedIndividuals.set(min, selectedIndividuals.get(i));
+            selectedIndividuals.set(i, aux);
 
-        result.addIndividual((I)bestIndividual.clone());
+        }
+        for (int i = 0; i < popSize; i++) {
+            result.addIndividual((I) selectedIndividuals.get(i).clone());
+        }
 
         return result;
     }
 
 
-    private I rankBased (Population<I, P> population)
+    /*private I rankBased (Population<I, P> population)
     {
         for (int i = 0; i < popSize; i++) {
 
@@ -43,7 +52,7 @@ public class RankBased<I extends Individual, P extends Problem<I>> extends Selec
         }
 
         return
-    }
+    }*/
 
     @Override
     public String toString(){
